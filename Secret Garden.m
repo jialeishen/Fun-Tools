14510e0befb8d83393f9c15c1ec71f258994d676
kidisgod <shenjialei1992@163.com>
2016-10-27 18:15:23 +0800

    Create Secret Garden.m

---
+++ Secret Garden.m
@@ -0,0 +1,14 @@
+%Paint secret garden with Matlab
+%author:Jialei Shen
+%e-mail:shenjialei1992@163.com
+%latest:2016.07.31
+
+clear
+clc
+I=imread('image.jpg');
+J=im2bw(I,0.95);
+[K,n]=bwlable(J,8);
+m=0.5+0.4*rand(n,3);  %random color range
+RGB=lable2rgb(K,m,'k');
+figure,
+imshow(RGB);
