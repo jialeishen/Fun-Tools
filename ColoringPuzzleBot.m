abdc9537567b84bab008563cc0db5b2c0700257f
Jialei Shen <shenjialei1992@163.com>
2022-03-18 12:30:20 -0400

    Update ColoringPuzzleBot.m

--- ColoringPuzzleBot.m
+++ ColoringPuzzleBot.m
@@ -1,11 +1,11 @@
-%Paint secret garden with Matlab
+%A bot completing coloring puzzles automatically. 
 %author:Jialei Shen
 %e-mail:jshen20@syr.edu
 %latest:2016.07.31
 
 clear
 clc
-I=imread('image.jpg');
+I=imread('image.jpg'); %import a black and white coloring puzzle image
 J=im2bw(I,0.95);
 [K,n]=bwlabel(J,8);
 m=0.5+0.4*rand(n,3);  %random color range
